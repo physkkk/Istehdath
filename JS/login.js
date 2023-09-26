de91f1a2142cac7be1da4a9cc289bb247bdd68ff
jia <jojoalanazi@icloud.com>
2023-09-26 15:52:50 +0300

    home update by aljawharah

--- JS/login.js
+++ JS/login.js
@@ -1,18 +1,14 @@
 // console.log('test');
 
-let namee = document.getElementById('name').value
-let b = document.getElementById('but')
-console.log(b, "edddd");
+//  namee = document.getElementById('name').value
+let b=document.getElementById('but')
+console.log( "edddd");
 b.addEventListener('click', function greet() {
-    all();
-})
-
-const all = () => {
     Swal.fire({
         position: 'top-end',
         icon: 'success',
         title: 'Your work has been saved',
         showConfirmButton: false,
-        timer: 1500
+        timer: 8000
     })
-}
+})
